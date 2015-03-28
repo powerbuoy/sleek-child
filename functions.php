4654c5347cc89e1d4e63cbc7ebc9e2d88c3d21ac
AL <powerbuoy@gmail.com>
2015-03-28 21:45:20 +0100

    Latest

--- functions.php
+++ functions.php
@@ -35,9 +35,9 @@ add_action('init', 'sleek_child_register_sidebars');
 
 function sleek_child_register_sidebars () {
 	sleek_register_sidebars(array(
-		'aside'		=> __('Aside', 'sleek'), 
-		'header'	=> __('Header', 'sleek'), 
-		'footer'	=> __('Footer', 'sleek')
+		'aside'		=> 'Aside', 
+		'header'	=> 'Header', 
+		'footer'	=> 'Footer'
 	));
 }
 
@@ -87,9 +87,9 @@ function sleek_child_setup_lang () {
 # add_action('init', 'sleek_add_excerpts_to_pages');
 
 # Add some fields to users
-# add_filter('user_contactmethods', 'alcom_add_user_fields');
+# add_filter('user_contactmethods', 'sleek_child_add_user_fields');
 
-function alcom_add_user_fields () {
+function sleek_child_add_user_fields () {
 	$fields['googleplus'] = __('Google+', 'sleek');
 	$fields['stackoverflow'] = __('StackOverflow', 'sleek');
 	$fields['github'] = __('GitHub', 'sleek');
@@ -98,12 +98,15 @@ function alcom_add_user_fields () {
 }
 
 # Short codes
-# add_action('init', 'alcom_register_shortcodes');
+# add_action('init', 'sleek_child_register_shortcodes');
 
-function alcom_register_shortcodes () {
+function sleek_child_register_shortcodes () {
 	# Include - include any module through [include mod=random-testimonial]
 	add_shortcode('include', 'sleek_shortcode_include_module');
 
 	# Get Posts short code, see sleek/inc/get-posts.php for details
 	# add_shortcode('get-posts', 'sleek_shortcode_get_posts');
+
+	# MarkdownFile
+	# add_shortcode('markdown-file', 'sleek_shortcode_markdown_file');
 }
