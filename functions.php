2070257d60a6e97d8c5f59d46bd493d0a53d151e
AL <powerbuoy@gmail.com>
2015-03-29 15:19:32 +0200

    Use file.version.css instead of ?version

--- functions.php
+++ functions.php
@@ -6,19 +6,21 @@ function sleek_child_register_css_js () {
 	# No need for jQuery 2015 :D
 	wp_deregister_script('jquery');
 
-	# Google Maps
-#	wp_register_script('google_maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false');
-#	wp_enqueue_script('google_maps');
+	# Theme Head/Foot JS
+	if (WP_DEBUG) {
+		wp_register_script('sleek_head', get_stylesheet_directory_uri() . '/js/head.php', array(), null);
+		wp_register_script('sleek_foot', get_stylesheet_directory_uri() . '/js/foot.php', array(), null, true);
+	}
+	else {
+		wp_register_script('sleek_head', get_stylesheet_directory_uri() . '/js/head.' . filemtime(get_stylesheet_directory() . '/js/head.js') . '.js', array(), null);
+		wp_register_script('sleek_foot', get_stylesheet_directory_uri() . '/js/foot.' . filemtime(get_stylesheet_directory() . '/js/foot.js') . '.js', array(), null, true);
+	}
 
-	# Theme JS
-#	wp_register_script('sleek_child_head', get_stylesheet_directory_uri() . '/js/head.php', array(), filemtime(get_stylesheet_directory() . '/js/head.js'), true);
-#	wp_enqueue_script('sleek_child_head');
-
-	wp_register_script('sleek_child_foot', get_stylesheet_directory_uri() . '/js/foot.php', array(), filemtime(get_stylesheet_directory() . '/js/foot.js'), true);
-	wp_enqueue_script('sleek_child_foot');
+	wp_enqueue_script('sleek_head');
+	wp_enqueue_script('sleek_foot');
 
 	# Theme CSS
-	wp_register_style('sleek_child', get_stylesheet_directory_uri() . '/css/all.css', array(), filemtime(get_stylesheet_directory() . '/css/all.css'));
+	wp_register_style('sleek_child', get_stylesheet_directory_uri() . '/css/all.' . filemtime(get_stylesheet_directory() . '/css/all.css') . '.css', array(), null);
 	wp_enqueue_style('sleek_child');
 }
 
