dcb457e1a58351bcd33daabd9cff9b64c637477d
AL <powerbuoy@gmail.com>
2016-05-02 16:42:57 +0200

    Latest SleekChild

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,11 @@ var paths = {
 var sleekSASS = require(__dirname + '/../sleek/gulp/sass.js');
 
 gulp.task('sass', ['icons'], function () {
-	sleekSASS(paths.sass + 'all.scss', paths.dest);
+	return sleekSASS(paths.sass + 'all.scss', paths.dest);
 });
 
 gulp.task('sass-only', function () {
-	sleekSASS(paths.sass + 'all.scss', paths.dest);
+	return sleekSASS(paths.sass + 'all.scss', paths.dest);
 });
 
 /**
@@ -26,11 +26,11 @@ gulp.task('sass-only', function () {
 var sleekIcons = require(__dirname + '/../sleek/gulp/icons.js');
 
 gulp.task('rewrite-icon-css', ['download-icons'], function () {
-	sleekIcons.rewriteCSS(paths.icons, paths.sass);
+	return sleekIcons.rewriteCSS(paths.icons, paths.sass);
 });
 
 gulp.task('download-icons', function () {
-	sleekIcons.download('icons.json', paths.icons);
+	return sleekIcons.download('icons.json', paths.icons);
 });
 
 gulp.task('icons', ['rewrite-icon-css']);
@@ -42,11 +42,11 @@ var sleekJS = require(__dirname + '/../sleek/gulp/js.js');
 var sleekJSHint = require(__dirname + '/../sleek/gulp/jshint.js');
 
 gulp.task('js', ['js-hint'], function () {
-	sleekJS(paths.js, paths.dest);
+	return sleekJS(paths.js, paths.dest);
 });
 
 gulp.task('js-hint', function () {
-	sleekJSHint(paths.js);
+	return sleekJSHint(paths.js);
 });
 
 /**
@@ -57,7 +57,7 @@ gulp.task('js-hint', function () {
 var sleekStyleguide = require(__dirname + '/../sleek/gulp/styleguide.js');
 
 gulp.task('styleguide', function () {
-	sleekStyleguide(paths.sass + 'all.scss', paths.dest);
+	return sleekStyleguide(paths.sass + 'all.scss', paths.dest);
 });
 
 /**
