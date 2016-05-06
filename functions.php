2c0389d1b84800212cf2922e314a425a41061e95
AL <powerbuoy@gmail.com>
2016-05-06 12:20:22 +0200

    Updated based on new sleek functions

--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
 <?php
-# Some config
+/**
+ * Some config (TODO: Move to Theme Options)
+ */
 define('RECAPTCHA_SITE_KEY', false);
 define('RECAPTCHA_SECRET', false);
 define('DISQUS_SHORTNAME', false);
 define('GOOGLE_ANALYTICS', false);
 
-# Thumbnails sizes
+/**
+ * Register thumbnail sizes
+ */
 # add_action('after_setup_theme', 'sleek_child_post_thumbnails');
 
 function sleek_child_post_thumbnails () {
@@ -13,40 +17,45 @@ function sleek_child_post_thumbnails () {
 	add_image_size('sleek-hd', 1920, 1080, true);
 }
 
-# Sidebars
+/**
+ * Register sidebars
+ */
 # add_action('init', 'sleek_child_register_sidebars');
 
 function sleek_child_register_sidebars () {
 	sleek_register_sidebars(array(
-		'aside'			=> __('Aside', 'sleek_child'),
-		'header'		=> __('Header', 'sleek_child'),
-		'footer'		=> __('Footer', 'sleek_child')
+		'aside'		=> array(
+			'name' => __('Aside', 'sleek_child'),
+			'before_title' => '<h3>',
+			'after_title' => '</h3>'
+		),
+		'header'	=> __('Header', 'sleek_child'),
+		'footer'	=> __('Footer', 'sleek_child')
 	));
 }
 
-# Custom post types and taxonomies
-# add_action('init', 'sleek_child_register_post_types');
+/**
+ * Register custom post types and taxonomies
+ */
+# add_action('init', 'sleek_child_register_post_types_and_taxonomies');
 
-function sleek_child_register_post_types () {
-	sleek_register_post_types(
-		# Post types (slug => description)
-		array(
-			'movies' => __('My movie collection', 'sleek_child'),
-			'directors' => __('My favorite directors', 'sleek_child')
-		),
+function sleek_child_register_post_types_and_taxonomies () {
+	sleek_register_post_types(array(
+		'movies' => __('My movie collection', 'sleek_child'),
+		'directors' => __('My favorite directors', 'sleek_child')
+	), 'sleek_child');
 
-		# Taxonomies and which post types they belong to
-		array(
-			'genres' => array('movies'),
-			'countries' => array('movies', 'directors')
-		),
-
-		# Translation textdomain (for URLs)
-		'sleek_child'
-	);
+	sleek_register_taxonomies(array(
+		'genres' => array('movies'),
+		'countries' => array('movies', 'directors')
+	), 'sleek_child');
 }
 
-# Register our CSS and JS
+/**
+ * Register CSS and JS
+ *
+ * TODO: Move to Sleek? (at least all.css/all.js?)
+ */
 add_action('wp_enqueue_scripts', 'sleek_child_register_css_js');
 
 function sleek_child_register_css_js () {
@@ -54,29 +63,36 @@ function sleek_child_register_css_js () {
 	wp_register_script('sleek_child', get_stylesheet_directory_uri() . '/dist/all.js?v=' . filemtime(get_stylesheet_directory() . '/dist/all.js'), array('jquery'), null, true);
 	wp_enqueue_script('sleek_child');
 
+	# Google Webfonts
+	# wp_register_style('sleek_child_font_lato', 'https://fonts.googleapis.com/css?family=Lato:300,900');
+	# wp_enqueue_style('sleek_child_font_lato');
+
 	# Theme CSS
 	wp_register_style('sleek_child', get_stylesheet_directory_uri() . '/dist/all.css?v=' . filemtime(get_stylesheet_directory() . '/dist/all.css'), array(), null);
 	wp_enqueue_style('sleek_child');
 }
 
-# Short codes
+/**
+ * Add optional shortcodes provided by SleekWP
+ *
+ * TODO: Move to individual plugins
+ */
 # add_action('init', 'sleek_child_register_shortcodes');
 
 function sleek_child_register_shortcodes () {
-	# Include - include any module through [include mod=random-testimonial]
-	add_shortcode('include', 'sleek_shortcode_include_module');
-
-	# Get Posts short code, see sleek/inc/get-posts.php for details
-	add_shortcode('get-posts', 'sleek_shortcode_get_posts');
+	# Include - include any module through [include mod=random-testimonial] (TODO: Change to get_template_part (but still allow arguments?))
+	# add_shortcode('include', 'sleek_shortcode_include_module');
 
 	# MarkdownFile
 	# add_shortcode('markdown-file', 'sleek_shortcode_markdown_file');
 
 	# Hubspot form
-	# add_shortcode('hubspot_form', 'sleek_hubspot_form');
+	# add_shortcode('hubspot-form', 'sleek_hubspot_form');
 }
 
-# Add some fields to users
+/**
+ * Add more fields to users
+ */
 # add_filter('user_contactmethods', 'sleek_child_add_user_fields');
 
 function sleek_child_add_user_fields () {
@@ -87,61 +103,74 @@ function sleek_child_add_user_fields () {
 	return $fields;
 }
 
-# Set up for translation (put your mo/po-files in your-theme/lang/ and uncomment this)
+/**
+ * Set up for translation (put your mo/po-files in your-theme/lang/)
+ */
 add_action('after_setup_theme', 'sleek_child_setup_lang');
 
 function sleek_child_setup_lang () {
 	load_child_theme_textdomain('sleek_child', get_stylesheet_directory() . '/lang');
 }
 
-# Disable "single" pages for certain post types
-# add_action('template_redirect', 'sleek_child_disable_single_post_types');
+/**
+ * These are optional actions to improve how WP normally does things
+ */
+# Give pages excerpts
+# add_action('init', 'sleek_add_excerpts_to_pages');
 
-function sleek_child_disable_single_post_types () {
-	sleek_disable_single_post_types(array('slides', 'employees', 'knowledge_base', 'testimonials', 'offices'));
-}
+# Add a favicon.ico if it exists in the theme directory
+add_action('wp_head', 'sleek_add_favicon');
 
-# Show all posts when browsing custom post types
-# add_filter('pre_get_posts', 'sleek_show_all_cpt_posts');
+# Adds a Browser Update script for older browsers
+# add_action('wp_head', 'sleek_register_browser_update_js');
+
+# Allow shortcodes in Widgets
+# add_action('init', 'sleek_allow_shortcodes_in_widgets');
 
 # Cleanup HEAD
 add_action('init', 'sleek_cleanup_head');
 
-# Allow Markdown in excerpts and advanced custom fields
-# add_action('init', 'sleek_more_markdown');
-
-# Upgrade Browser warning for old versions of IE etc
-# add_action('wp_head', 'sleek_register_browser_update_js');
-
-# Show all post types when browsing author
-# add_filter('pre_get_posts', 'sleek_show_all_post_types_for_author');
-
-# Remove HOME from Yoast Breadcrumbs
-# add_filter('wpseo_breadcrumb_links', 'sleek_remove_home_from_breadcrumb');
-
-# Give pages excerpts
-# add_action('init', 'sleek_add_excerpts_to_pages');
-
-# Remo Emoji CSS/JS from head
-add_action('init', 'sleek_remove_emoji_css_js');
-
-# Disable jQuery NoConflict
-# add_action('wp_head', 'sleek_disable_jquery_noconflict');
-
-# Move jQuery to bottom of page
+# Move jQuery to bottom of page + include from CDN
 add_action('wp_enqueue_scripts', 'sleek_enqueue_jquery_cdn_in_footer');
 
-# Add OpenGraph
+# Allow Markdown in excerpts and ACF
+# add_action('init', 'sleek_more_markdown');
+
+# Add open graph tags to posts
 # add_action('wp_head', 'sleek_open_graph_tags');
 
-# Add Favicon
-add_action('wp_head', 'sleek_add_favicon');
+# Remove Emoji CSS/JS from head added since WP 4.2.2 (TODO: Doesn't work??)
+add_action('init', 'sleek_remove_emoji_css_js');
 
-# Add placeholders to comment form
-add_filter('comment_form_defaults', 'sleek_comment_form_placeholders');
+/**
+ * These are optional filters to improve how WP normally does things
+ */
+# Add an "active-parent" class to archive pages when browsing their taxonomies
+add_filter('nav_menu_css_class', 'sleek_active_archive_link_on_taxonomies', 10, 2);
 
 # Allow SVG Uploads
 # add_filter('upload_mimes', 'sleek_allow_svg_uploads');
 
+# Add placeholders to comment form
+add_filter('comment_form_defaults', 'sleek_comment_form_placeholders');
+
 # Disable Ultimate Post Widget CSS
 # add_filter('upw_enqueue_styles', 'sleek_disable_upw_styles');
+
+# Excludes the currently viewed post in UPW
+# add_filter('upw_wp_query_args', 'sleek_exclude_current_post_in_upw');
+
+# Allow HTML in Widget Titles (with [tags])
+# add_filter('widget_title', 'sleek_html_in_widget_titles');
+
+# Remove HOME from Yoast Breadcrumbs
+# add_filter('wpseo_breadcrumb_links', 'sleek_remove_home_from_breadcrumb');
+
+# Show all posts (ignore posts_per_page setting) when browsing custom post types
+# add_filter('pre_get_posts', 'sleek_show_all_cpt_posts');
+
+# Show all post types when browsing author
+# add_filter('pre_get_posts', 'sleek_show_all_post_types_for_authors');
+
+# Remove .current_page_parent from Blog-page when viewing another archive
+# add_filter('nav_menu_css_class', 'sleek_unset_active_blog_class', 10, 2);
