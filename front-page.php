69c74fc38ae9517760644da8a18a37c9c0cc4fb1
AL <powerbuoy@gmail.com>
2016-09-27 02:51:53 +0200

    Better

--- front-page.php
+++ front-page.php
@@ -2,12 +2,10 @@
 
 <main>
 
-	<pre>
-		<?php include get_template_directory() . '/README.md' ?>
-	</pre>
-
 	<?php get_template_part('modules/my-module') ?>
 	<?php get_template_part('modules/post-content') ?>
+
+	<pre><?php include get_template_directory() . '/README.md' ?></pre>
 
 </main>
 
