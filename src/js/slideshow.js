885ce83ae88ed85e681ab72c00ff1c8577b1e383
AL <powerbuoy@gmail.com>
2017-05-31 16:35:22 +0200

    Much better

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -5,6 +5,30 @@
 
 	require('slick-carousel');
 
+	// Wrap WP galleries in [data-slideshow]
+	$('div.gallery').each(function () {
+		var gallery = $(this).addClass('is-slideshow');
+		var colClass = gallery.attr('class').split(' ').filter(function (val) {
+			return val.indexOf('gallery-columns-') != -1;
+		});
+		var numCols = 1;
+
+		if (colClass && colClass.length) {
+			numCols = colClass[0].replace('gallery-columns-', '');
+		}
+
+		gallery.wrapInner('<div data-slideshow="' + numCols + '"></div>');
+	});
+
+	// NOTE: These should match the SASS variables in config.scss
+	var siteWidthMax = 1360;
+	var siteWidth = 90;
+	var bpMedium = 760;
+
+	var sitePadding = (100 - siteWidth) / 2 + '%';
+	var siteWidthReached = (siteWidthMax * 100) / siteWidth;
+
+	// Find every [data-slideshow]
 	$('[data-slideshow]').each(function () {
 		var wrap = $(this);
 		var id = wrap.attr('id');
@@ -12,20 +36,57 @@
 		var slides = wrap.find('> *');
 		var numSlides = wrap.attr('data-slideshow');
 			numSlides = numSlides ? parseInt(numSlides) : 1;
+		var config = {
+			dots: true,
+			arrows: false,
+			slidesToShow: numSlides,
+			slidesToScroll: numSlides,
+			infinite: true,
+			speed: 400,
+			autoplay: false,
+		//	adaptiveHeight: true,
+			prevArrow: '<a role="button" class="slick-prev icon-left"></a>',
+			nextArrow: '<a role="button" class="slick-next icon-right"></a>'
+		};
 
-		if (slides.length > numSlides) {
-			wrap.slick({
-				dots: true,
-				arrows: true,
-				slidesToShow: numSlides,
-				slidesToScroll: numSlides,
-				infinite: true,
-				speed: 400,
-				autoplay: false,
-				adaptiveHeight: true,
-				prevArrow: '<a role="button" class="slick-prev icon-left"></a>',
-				nextArrow: '<a role="button" class="slick-next icon-right"></a>'
+		// Center mode!
+		if (wrap.is('[data-slideshow-center-mode]')) {
+			config.centerMode = true;
+			config.centerPadding = (($(window).width() - siteWidthMax) / 2) + 'px';
+			config.slidesToShow = numSlides;
+
+			// In low res
+			config.responsive = [
+				{
+					breakpoint: bpMedium,
+					settings: {
+						slidesToShow: 1,
+						centerPadding: sitePadding
+					}
+				},
+				{
+					breakpoint: siteWidthReached,
+					settings: {
+						slidesToShow: numSlides,
+						centerPadding: sitePadding
+					}
+				}
+			];
+
+			// On resize - re-calculate centerPadding
+			$(window).on('resize', function () {
+				if (window.matchMedia('(min-width: ' + siteWidthReached + 'px)').matches) {
+					wrap.slick('slickSetOption', 'centerPadding', (($(window).width() - siteWidthMax) / 2) + 'px');
+				}
+				else {
+					wrap.slick('slickSetOption', 'centerPadding', sitePadding);
+				}
 			});
+		}
+
+		// Init slideshow
+		if (slides.length > numSlides) {
+			wrap.slick(config);
 
 			if (nav.length) {
 				// Select the first
