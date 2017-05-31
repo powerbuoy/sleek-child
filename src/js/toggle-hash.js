f34ac8f4d59a3644746fc896dabf162c73417eeb
AL <powerbuoy@gmail.com>
2017-06-01 00:49:56 +0200

    Taths better

--- src/js/toggle-hash.js
+++ src/js/toggle-hash.js
@@ -5,12 +5,22 @@
 
 	require('toggle-hash');
 
-	$('[data-toggle-hash]').toggleHash({
-		onAdd: function (hash) {
-			$(document.documentElement).addClass('hash-active').addClass('hash-active-' + hash.substr(1));
-		},
-		onRemove: function (hash) {
-			$(document.documentElement).removeClass('hash-active').removeClass('hash-active-' + hash.substr(1));
-		},
+	$('[data-toggle-hash]').each(function () {
+		var t = $(this);
+		var tText = t.text();
+		var ttText = t.attr('data-toggle-hash');
+
+		ttText = ttText ? ttText : tText;
+
+		t.toggleHash({
+			onAdd: function (hash) {
+				t.text(ttText);
+				$(document.documentElement).addClass('hash-active').addClass('hash-active-' + hash.substr(1));
+			},
+			onRemove: function (hash) {
+				t.text(tText);
+				$(document.documentElement).removeClass('hash-active').removeClass('hash-active-' + hash.substr(1));
+			},
+		});
 	});
 })();
