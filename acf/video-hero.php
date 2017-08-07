6bb9a006daa3ce4d687d512ad60dd038443efb30
AL <powerbuoy@gmail.com>
2017-08-07 15:34:49 +0200

    Some tweaks

--- acf/video-hero.php
+++ acf/video-hero.php
@@ -3,7 +3,6 @@
 		'name' => 'video-hero-code',
 		'label' => __('Video', 'sleek_child'),
 		'instructions' => __('Copy the YouTube/Vimeo URL and paste it here.', 'sleek_child'),
-		'type' => 'oembed',
-		'required' => true
+		'type' => 'oembed'
 	]
 ];
