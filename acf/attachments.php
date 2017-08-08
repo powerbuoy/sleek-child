46898220d2eb2253d966e7ce755df2f926a77272
AL <powerbuoy@gmail.com>
2017-08-08 12:23:32 +0200

    New acf: attachments

---
+++ acf/attachments.php
@@ -0,0 +1,29 @@
+<?php
+return [
+	[
+		'name' => 'attachments-title',
+		'label' => __('Title', 'sleek_child'),
+		'instructions' => __('Enter a title to display above the attachments.', 'sleek_child'),
+		'type' => 'text'
+	],
+	[
+		'name' => 'attachments-description',
+		'label' => __('Description', 'sleek_child'),
+		'instructions' => __('Enter a description for the attachments.', 'sleek_child'),
+		'type' => 'wysiwyg'
+	],
+	[
+		'name' => 'attachments-files',
+		'label' => __('Files', 'sleek_child'),
+		'instructions' => __('Select any number of files.', 'sleek_child'),
+		'type' => 'repeater',
+		'required' => true,
+		'sub_fields' => [
+			[
+				'name' => 'attachments-files-file',
+				'label' => __('Select a file', 'sleek_child'),
+				'type' => 'file'
+			]
+		]
+	]
+];
