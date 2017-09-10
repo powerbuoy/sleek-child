2ca72b0017605b23a971546e3fd133011a6913da
AL <powerbuoy@gmail.com>
2017-09-10 13:03:41 +0200

    Better

--- src/js/toggle-hash.js
+++ src/js/toggle-hash.js
@@ -9,16 +9,23 @@
 		var t = $(this);
 		var tText = t.text();
 		var ttText = t.attr('data-toggle-hash');
+		var hasText = ttText ? true : false;
 
 		ttText = ttText ? ttText : tText;
 
 		t.toggleHash({
 			onAdd: function (hash) {
-				t.text(ttText);
+				if (hasText) {
+					t.text(ttText);
+				}
+
 				$(document.documentElement).addClass('hash-active').addClass('hash-active-' + hash.substr(1));
 			},
 			onRemove: function (hash) {
-				t.text(tText);
+				if (hasText) {
+					t.text(tText);
+				}
+
 				$(document.documentElement).removeClass('hash-active').removeClass('hash-active-' + hash.substr(1));
 			},
 		});
