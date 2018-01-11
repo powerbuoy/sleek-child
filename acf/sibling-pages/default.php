67559dc8180d88c2136deed040505b86372ceaf0
AL <powerbuoy@gmail.com>
2018-01-11 23:21:11 +0100

    Check redirect-url and set target=_blank

--- acf/sibling-pages/default.php
+++ acf/sibling-pages/default.php
@@ -29,24 +29,26 @@
 		<?php endif ?>
 
 		<?php foreach ($rows as $post) : setup_postdata($post) ?>
-			<?php # TODO: If redirect-url is set - open links in new windows ?>
+			<?php $target = get_field('redirect-url') ? 'target="_blank"' : '' ?>
 			<article>
 
 				<?php if (has_post_thumbnail()) : ?>
 					<figure>
-						<a href="<?php the_permalink() ?>">
+						<a href="<?php the_permalink() ?>" <?php echo $target ?>>
 							<?php the_post_thumbnail('medium') ?>
 						</a>
 					</figure>
 				<?php endif ?>
 
-				<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+				<h3>
+					<a href="<?php the_permalink() ?>" <?php echo $target ?>>
+						<?php the_title() ?>
+					</a>
+				</h3>
 
 				<?php the_excerpt() ?>
 
-				<?php sleek_get_template_part('acf/buttons/default', ['data' => ['buttons' => get_field('buttons')]]) ?>
-
-				<a href="<?php the_permalink() ?>"><?php _e('Read more', 'sleek') ?></a>
+				<a href="<?php the_permalink() ?>" <?php echo $target ?>><?php _e('Read more', 'sleek') ?></a>
 
 			</article>
 		<?php endforeach; wp_reset_postdata() ?>
