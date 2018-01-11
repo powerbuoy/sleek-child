c5f57d662cacd9b39d109d9ace5820383feceb8a
AL <powerbuoy@gmail.com>
2018-01-11 23:22:49 +0100

    Check redirect url and set target

--- acf/featured-posts/default.php
+++ acf/featured-posts/default.php
@@ -15,22 +15,26 @@
 	<?php endif ?>
 
 	<?php foreach ($data['featured-posts-posts'] as $post) : setup_postdata($post) ?>
-		<?php # TODO: If redirect-url is set - open links in new windows ?>
+		<?php $target = get_field('redirect-url') ? 'target="_blank"' : '' ?>
 		<article>
 
 			<?php if (has_post_thumbnail()) : ?>
 				<figure>
-					<a href="<?php the_permalink() ?>">
+					<a href="<?php the_permalink() ?>" <?php echo $target ?>>
 						<?php the_post_thumbnail('medium') ?>
 					</a>
 				</figure>
 			<?php endif ?>
 
-			<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+			<h3>
+				<a href="<?php the_permalink() ?>" <?php echo $target ?>>
+					<?php the_title() ?>
+				</a>
+			</h3>
 
 			<?php the_excerpt() ?>
 
-			<a href="<?php the_permalink() ?>"><?php _e('Read more', 'sleek') ?></a>
+			<a href="<?php the_permalink() ?>" <?php echo $target ?>><?php _e('Read more', 'sleek') ?></a>
 
 		</article>
 	<?php endforeach; wp_reset_postdata() ?>
