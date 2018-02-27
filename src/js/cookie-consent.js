232d6f1659630d960247e4b71875c24a03248a3b
AL <powerbuoy@gmail.com>
2018-02-27 15:53:04 +0100

    Break out cookie consent styling and copy

--- src/js/cookie-consent.js
+++ src/js/cookie-consent.js
@@ -1,30 +1,21 @@
 (function () {
 	'use strict';
 
-	var acceptsCookies = window.localStorage.getItem('accepts_cookies');
-	var cookieMessage = 'We use cookies to bring you the best possible experience when browsing our site. <a href="https://cookiesandyou.com/" target="_blank">Read more about cookies</a> | <a href="#" class=" close">Accept</a>';
+	var acceptsCookies = window.localStorage.getItem('cookie_consent');
+	var cookieMessage = sleek_child_config.COOKIE_CONSENT;
 
 	if (!acceptsCookies) {
-		var warning = document.createElement('div');
+		var el = document.createElement('div');
 
-		warning.id = 'cookie-warning';
-		warning.innerHTML = cookieMessage;
+		el.id = 'cookie-consent';
+		el.innerHTML = cookieMessage;
 
-		warning.style.background = 'rgba(255, 255, 255, .95)';
-		warning.style.position = 'fixed';
-		warning.style.left = 0;
-		warning.style.right = 0;
-		warning.style.bottom = 0;
-		warning.style.zIndex = 1000;
-		warning.style.padding = '1.5rem';
-		warning.style.textAlign = 'center';
+		document.body.appendChild(el);
 
-		document.body.appendChild(warning);
-
-		warning.querySelector('a.close').addEventListener('click', function (e) {
+		el.querySelector('a.close').addEventListener('click', function (e) {
 			e.preventDefault();
-			window.localStorage.setItem('accepts_cookies', true);
-			warning.parentNode.removeChild(warning);
+			window.localStorage.setItem('cookie_consent', true);
+			el.parentNode.removeChild(el);
 		});
 	}
 })();
